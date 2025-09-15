ea1c1d7468d7409ef13c3a9af0d0688c00a463df
Sowdarjya <kolaysowdarjya@gmail.com>
2025-09-15 06:48:50 +0000

    Passing query params

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use std::net::SocketAddr;
-use axum::{response::Html, routing::get, Router};
+
+use axum::{extract::Query, response::Html, routing::get, Router};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(|| async { Html("Hello, <strong>World!</strong>") }));
-    
+    let routes_hello = Router::new().route("/hello", get(hello));
+
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
-    println!("listening on {}", addr);
-    
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("listening on {}", addr);
     axum::serve(listener, routes_hello).await.unwrap();
 }
+
+async fn hello(Query(params): Query<HelloParams>) -> impl axum::response::IntoResponse {
+    println!("{:<12} - Hello World!", "GET /hello");
+
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("<h1>Hello, {name}</h1>"))
+}
+
